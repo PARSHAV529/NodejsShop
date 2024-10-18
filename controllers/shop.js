a19ce8ff0ab4819980ba1c62a379155b97d02098
Parshav <parshav143@gmail.com>
2024-10-18 19:55:12 +0530

    add all files

---
+++ controllers/shop.js
@@ -0,0 +1,62 @@
+const Product = require('../models/product');
+
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll(products => {
+    res.render('shop/product-list', {
+      prods: products,
+      pageTitle: 'All Products',
+      path: '/products'
+    });
+  });
+};
+
+exports.getProduct = (req, res, next) => {
+  const productId = req.params.Id
+  Product.findById(productId,prod=>{
+    res.render('shop/product-detail',{
+      product: prod,
+      pageTitle:prod.title,
+      path: `/products`
+    })
+  })
+  
+  
+}
+
+exports.getIndex = (req, res, next) => {
+  Product.fetchAll(products => {
+    res.render('shop/index', {
+      prods: products,
+      pageTitle: 'Shop',
+      path: '/'
+    });
+  });
+};
+
+exports.getCart = (req, res, next) => {
+  res.render('shop/cart', {
+    path: '/cart',
+    pageTitle: 'Your Cart'
+  });
+};
+
+exports.postCart = (req, res, next) => {
+  const productId = req.body.productId
+  console.log(productId);
+  res.redirect('/cart')
+  
+}
+
+exports.getOrders = (req, res, next) => {
+  res.render('shop/orders', {
+    path: '/orders',
+    pageTitle: 'Your Orders'
+  });
+};
+
+exports.getCheckout = (req, res, next) => {
+  res.render('shop/checkout', {
+    path: '/checkout',
+    pageTitle: 'Checkout'
+  });
+};
